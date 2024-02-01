f384980c42287dba9e3bb2b0834a85d5356822f8
cgjeffri <cgjeffri@asu.edu>
2024-02-01 00:20:14 -0700

    Task-48: update httpclient timeout to 5 seconds

--- src/main/java/taiga/util/HTTPClientSingleton.java
+++ src/main/java/taiga/util/HTTPClientSingleton.java
@@ -13,7 +13,7 @@ public class HTTPClientSingleton {
      */
     public static HttpClient getInstance() {
         if (client == null) {
-            client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(20)).build();
+            client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(5)).build();
         }
         return client;
     }
