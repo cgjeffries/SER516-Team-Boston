934e7831f919ce8b897cbf400cc14280b77a42d8
Battlesquid <esanoluwamayowa@gmail.com>
2024-02-03 22:20:24 -0700

    Task 125: fix tests

--- src/test/java/settings/SettingsTest.java
+++ src/test/java/settings/SettingsTest.java
@@ -16,12 +16,31 @@ import settings.appmodel.AppModel;
 import taiga.model.query.project.Project;
 
 public class SettingsTest {
+
     private final String TEST_SETTINGS_FILE = "test_settings.json";
 
+    private AppModel appModel;
+
+    private Project p1;
+    private Project p2;
+    private Project p3;
     @BeforeEach
     public void setUp() {
         System.setProperty("user.home", System.getProperty("user.dir"));
         deleteTestSettingsFile();
+
+        appModel = new AppModel();
+        // the internal list of projects is lazy loaded, so we do this to initialize it
+        appModel.getProjects();
+        p1 = new Project();
+        p1.setSlug("project-one");
+        p1.setId(1);
+        p2 = new Project();
+        p2.setSlug("project-two");
+        p2.setId(2);
+        p3 = new Project();
+        p3.setSlug("project-three");
+        p3.setId(3);
     }
 
     @AfterEach
@@ -32,11 +51,10 @@ public class SettingsTest {
     @Test
     public void saveAndLoadSettings() {
         Settings settings = Settings.get();
-        AppModel appModel = new AppModel();
         // TODO: maybe add some utility wrapper for easily setting the slug and id
-        appModel.addProject(new Project());
-        appModel.addProject(new Project());
-        appModel.setCurrentProject(new Project());
+        appModel.addProject(p1);
+        appModel.addProject(p2);
+        appModel.setCurrentProject(p3);
         settings.appModel = appModel;
 
         settings.save();
