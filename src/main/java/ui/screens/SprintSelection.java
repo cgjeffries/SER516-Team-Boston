6e61eb95ef8dd7264768f09fc9c2fec263536f87
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2024-02-04 23:15:31 -0700

    Task 132: begin sprint selection screen

---
+++ src/main/java/ui/screens/SprintSelection.java
@@ -0,0 +1,37 @@
+package ui.screens;
+
+import javafx.scene.layout.VBox;
+import ui.components.Screen;
+import ui.util.ScreenManager;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SprintSelection extends Screen<VBox> {
+    private static final VBox root = new VBox();
+
+    /**
+     * Create a screen instance
+     *
+     * @param screenManager a ScreenManager instance
+     * @param name          A unique name for the scene.
+     */
+    public SprintSelection(ScreenManager screenManager, String name) {
+        super(screenManager, name);
+    }
+
+    @Override
+    public VBox getRoot() {
+        return root;
+    }
+
+    @Override
+    public Object getController() {
+        return this;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
