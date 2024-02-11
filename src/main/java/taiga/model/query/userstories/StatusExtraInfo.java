61f42c4f3b55fb1cf8641934bd08a4c5d4c796dc
jthayer4 <jthayer4@asu.edu>
2024-02-10 17:46:41 -0700

    T-77 Added model for Userstories

---
+++ src/main/java/taiga/model/query/userstories/StatusExtraInfo.java
@@ -0,0 +1,44 @@
+package taiga.model.query.userstories;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class StatusExtraInfo {
+
+    @SerializedName("color")
+    @Expose
+    private String color;
+
+    @SerializedName("is_closed")
+    @Expose
+    private Boolean isClosed;
+    
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getIsClosed() {
+        return isClosed;
+    }
+
+    public void setIsClosed(Boolean isClosed) {
+        this.isClosed = isClosed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
