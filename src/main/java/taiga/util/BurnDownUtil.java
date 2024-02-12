145ab04bcefe8feb93a9cfb84e5e2f23132dfc6b
cgjeffri <cgjeffri@asu.edu>
2024-02-11 20:08:33 -0700

    Task 74: WiP BV burndown calculation

--- src/main/java/taiga/util/BurnDownUtil.java
+++ src/main/java/taiga/util/BurnDownUtil.java
@@ -1,6 +1,15 @@
 package taiga.util;
 
 
+import static taiga.util.UserStoryUtils.extractBusinessValue;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import javafx.util.Pair;
 import taiga.api.UserStoryAPI;
 import taiga.model.query.sprint.Sprint;
 import taiga.model.query.sprint.UserStory;
@@ -31,7 +40,7 @@ public class BurnDownUtil {
         UserStoryUtils userStoryUtils = new UserStoryUtils();
         double tempTotal = 0;
         for(UserStoryDetail story : userStories) {
-            tempTotal += userStoryUtils.extractBusinessValue(story);
+            tempTotal += extractBusinessValue(story);
         }
         return tempTotal;
     }
@@ -42,8 +51,44 @@ public class BurnDownUtil {
         });
     }
 
-    private void calculateUserStoryBurndown() {
+    private LocalDate convertToLocalDate(Date date){
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
 
+    class BurndownEntry{
+        BurndownEntry(LocalDate date, Double value){
+            this.date = date;
+            this.value = value;
+        }
+        LocalDate date;
+        Double value;
+    }
+
+    private List<BurndownEntry> calculateUserStoryBurndown() {
+        List<UserStoryDetail> userStories = null; //TODO: actually get the list of user stories
+
+        LocalDate sprintStartLocalDate = convertToLocalDate(this.sprint.getEstimatedStart());
+
+        LocalDate sprintEndLocalDate = convertToLocalDate(this.sprint.getEstimatedFinish());
+
+        List<LocalDate> sprintDates = sprintStartLocalDate.datesUntil(sprintEndLocalDate).toList();
+
+        List<BurndownEntry> burndown = new ArrayList<>();
+
+        for(int i = 0; i < sprintDates.size(); i++){
+            Double value = this.businessValueTotal;
+            if(i != 0){
+                value = burndown.get(i-1).value;
+            }
+            for(UserStoryDetail userStoryDetail : userStories){
+                if(convertToLocalDate(userStoryDetail.getFinishDate()).equals(sprintDates.get(i))){
+                    value = value - extractBusinessValue(userStoryDetail);
+                }
+            }
+            burndown.add(new BurndownEntry(sprintDates.get(i), value));
+        }
+
+        return burndown;
     }
 
     public Sprint getSprint() {
