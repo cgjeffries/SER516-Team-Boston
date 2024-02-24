258d9637ee493be8a4cdf52dc8615ee9b65c3826
mjkangas1 <mjkangas@asu.edu>
2024-02-24 12:12:08 -0700

    T-254 Added epic handling, provided getters and setters to use helper values

--- src/main/java/taiga/util/pbChange/PBHelper.java
+++ src/main/java/taiga/util/pbChange/PBHelper.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
+import taiga.api.EpicsAPI;
 import taiga.api.SprintAPI;
 import taiga.api.UserStoryAPI;
+import taiga.model.query.epic.EpicDetail;
 import taiga.model.query.sprint.Sprint;
 import taiga.model.query.sprint.UserStoryDetail;
 
 public class PBHelper {
 
-    List<Sprint> sprintList;
-    List<UserStoryDetail> userStoryList;
+    private List<Sprint> sprintList;
+    private List<UserStoryDetail> userStoryList;
+    private List<EpicDetail> epicList;
+
     UserStoryAPI userStoryAPI = new UserStoryAPI();
     SprintAPI sprintAPI = new SprintAPI();
+    EpicsAPI epicAPI = new EpicsAPI();
     
     public PBHelper(Integer projectId, Integer sprintID) {
         AtomicReference<List<UserStoryDetail>> userStoryListReference = new AtomicReference<>();
@@ -31,8 +36,38 @@ public class PBHelper {
 
         sprintList = sprintListReference.get();
 
+        AtomicReference<List<EpicDetail>> epicListReference = new AtomicReference<>();
+        epicAPI.listEpics( result ->{
+            epicListReference.set(new ArrayList<>(List.of(result.getContent())));
+        }).join();
+
+        epicList = epicListReference.get();
+
     }
 
+    public List<Sprint> getSprintList() {
+        return sprintList;
+    }
+
+    public void setSprintList(List<Sprint> sprintList) {
+        this.sprintList = sprintList;
+    }
+
+    public List<UserStoryDetail> getUserStoryList() {
+        return userStoryList;
+    }
+
+    public void setUserStoryList(List<UserStoryDetail> userStoryList) {
+        this.userStoryList = userStoryList;
+    }
+
+    public List<EpicDetail> getEpicList() {
+        return epicList;
+    }
+
+    public void setEpicList(List<EpicDetail> epicList) {
+        this.epicList = epicList;
+    }
 
 }
 
