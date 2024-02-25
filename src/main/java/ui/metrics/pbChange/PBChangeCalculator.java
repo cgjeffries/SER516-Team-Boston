f12ed9d78adc5ab3f474f96b8be15b30f49bebb1
mjkangas1 <mjkangas@asu.edu>
2024-02-24 23:10:03 -0700

    T-257 Removed the PBHelper class and kept needed functiosn in PBChangeCalculator

--- src/main/java/ui/metrics/pbChange/PBChangeCalculator.java
+++ src/main/java/ui/metrics/pbChange/PBChangeCalculator.java
@@ -2,13 +2,30 @@ package ui.metrics.pbChange;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+import settings.Settings;
+import taiga.api.UserStoryAPI;
 import taiga.model.query.epic.EpicDetail;
 import taiga.model.query.sprint.Sprint;
 import taiga.model.query.sprint.UserStoryDetail;
 import taiga.model.query.userstories.UserStoryInterface;
 
 public class PBChangeCalculator {
-    public static List<UserStoryInterface> filterUSAfterSprintStart(List<UserStoryInterface> userStories, Sprint sprint){
+    private static List<UserStoryDetail> userStories;
+
+    private UserStoryAPI userStoryAPI = new UserStoryAPI();
+
+    public PBChangeCalculator() {
+        AtomicReference<List<UserStoryDetail>> userStoryListReference = new AtomicReference<>();
+        userStoryAPI.listProjectUserStories(Settings.get().getAppModel().getCurrentProject().get().getId(), result -> {
+            userStoryListReference.set(new ArrayList<>(List.of(result.getContent())));
+        }).join();
+
+        userStories = userStoryListReference.get();
+    }
+
+    public static List<UserStoryInterface> filterUSAfterSprintStart(Sprint sprint){
         List<UserStoryInterface> addedAfterStart = new ArrayList<>();
         for(UserStoryInterface userStory : userStories){
             if(userStory.getCreatedDate().after(sprint.getEstimatedStart())){
@@ -28,7 +45,7 @@ public class PBChangeCalculator {
         return addedAfterStart;
     }
 
-    public static List<UserStoryDetail> filterUSRemovedAfterSprintStart(List<UserStoryDetail> userStories, Sprint sprint) {
+    public static List<UserStoryDetail> filterUSRemovedAfterSprintStart(Sprint sprint) {
         List<UserStoryDetail> removedAfterStart = new ArrayList<>();
         for (UserStoryDetail userStory : userStories) {
             if (userStory.getMilestone() != null) {
