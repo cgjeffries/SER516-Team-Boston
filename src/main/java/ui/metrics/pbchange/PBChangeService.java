69333fd86093f6cd09e1fa3ac40741ef756a896f
charles <sazrocks6@gmail.com>
2024-02-26 23:59:24 -0700

    Task-258: WIP service implementation

--- src/main/java/ui/metrics/pbchange/PBChangeService.java
+++ src/main/java/ui/metrics/pbchange/PBChangeService.java
@@ -1,12 +1,24 @@
 package ui.metrics.pbchange;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import javafx.collections.ObservableList;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
+import javafx.scene.chart.XYChart;
+import taiga.api.UserStoryAPI;
 import taiga.model.query.sprint.Sprint;
+import taiga.model.query.sprint.UserStory;
+import taiga.model.query.sprint.UserStoryDetail;
+import taiga.model.query.taskhistory.ItemHistory;
+import taiga.model.query.userstories.UserStoryInterface;
 
 
 public class PBChangeService extends Service<Object> {
     private Sprint sprint;
+
+    private UserStoryAPI userStoryAPI;
 
     public PBChangeService() {
     }
@@ -24,6 +36,21 @@ public class PBChangeService extends Service<Object> {
         if (exception != null) {
             exception.printStackTrace();
         }
+    }
+
+    private void updatePBChanges(ObservableList<XYChart.Data<String, Number>> data){
+        AtomicReference<List<UserStoryDetail>> userStoryListReference = new AtomicReference<>();
+        userStoryAPI.listProjectUserStories(sprint.getProject(), result -> {
+            userStoryListReference.set(new ArrayList<>(List.of(result.getContent())));
+        }).join();//TODO: why can't this be UserStoryInterface?
+
+        for (UserStoryDetail userStoryDetail : userStoryListReference.get()){
+            if(true) { //TODO: link to util code
+                //data.add(userStoryDetail.getId(), userStoryDetail.getCreatedDate().getTime());
+            }
+        }
+
+
     }
 
 
