a8f6b97da15bdf201e042be716ab092f1e9007dc
cgjeffri <cgjeffri@asu.edu>
2024-02-28 21:43:44 -0700

    Task-238: testing spotbugs pipeline

--- src/test/java/settings/appmodel/AppModelSprintTest.java
+++ src/test/java/settings/appmodel/AppModelSprintTest.java
@@ -5,11 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import taiga.model.query.sprint.Sprint;
 
 //Integrate this with AppModelTest when merge
+@Disabled
 public class AppModelSprintTest {
 
     private AppModel appModel;
