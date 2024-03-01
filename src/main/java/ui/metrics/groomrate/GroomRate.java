8b4c2f3a9d035fe10a6d92100f0aa5be31d50eb3
mayowaaaaa <oesan@asu.edu>
2024-02-29 22:21:51 -0700

    Task 178: add groom rate component

---
+++ src/main/java/ui/metrics/groomrate/GroomRate.java
@@ -0,0 +1,67 @@
+package ui.metrics.groomrate;
+
+import atlantafx.base.controls.Spacer;
+import atlantafx.base.theme.Styles;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.geometry.Pos;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.util.Date;
+
+public class GroomRate extends StackPane {
+
+    private final ReadOnlyObjectProperty<Date> endDate;
+    private final ReadOnlyObjectProperty<Date> startDate;
+
+    public GroomRate(ReadOnlyObjectProperty<Date> startDate, ReadOnlyObjectProperty<Date> endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.init();
+    }
+    private void init() {
+        VBox.setVgrow(this, Priority.ALWAYS);
+
+        StackPane root = new StackPane();
+        VBox.setVgrow(root, Priority.ALWAYS);
+
+        Text groomRate = new Text("0.500");
+        groomRate.getStyleClass().add(Styles.TITLE_1);
+
+
+        Text groomRateDesc = new Text("Ratio of Stories groomed between 02/20/2024 and 02/29/2024");
+
+        Spacer spacer = new Spacer(10);
+
+        Text modifiedLabel = new Text("6 modified");
+        modifiedLabel.getStyleClass().add(Styles.TITLE_4);
+
+        Text totalLabel = new Text("12 total stories");
+        totalLabel.getStyleClass().add(Styles.TITLE_4);
+
+        VBox container = new VBox(
+                groomRate,
+                groomRateDesc,
+                spacer,
+                modifiedLabel,
+                totalLabel
+        );
+        container.setPrefHeight(VBox.USE_PREF_SIZE);
+        container.setAlignment(Pos.CENTER);
+        VBox.setVgrow(container, Priority.ALWAYS);
+
+        ProgressIndicator progress = new ProgressIndicator(-1d);
+
+//        root.getChildren().add(progress);
+        root.getChildren().add(container);
+
+        getChildren().add(root);
+    }
+
+    public void recalculate(Date start, Date end) {
+
+    }
+}
