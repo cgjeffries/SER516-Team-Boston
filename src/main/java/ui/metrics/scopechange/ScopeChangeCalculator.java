05591967d5bbbcd0d4f43bb0029f1b8a75887223
mjkangas1 <mjkangas@asu.edu>
2024-03-01 18:02:11 -0700

    T-180 Removed Project calls in the methods as not used, added Javadoc comments to complicated methods

--- src/main/java/ui/metrics/scopechange/ScopeChangeCalculator.java
+++ src/main/java/ui/metrics/scopechange/ScopeChangeCalculator.java
@@ -7,7 +7,6 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 
 import taiga.api.HistoryAPI;
-import taiga.model.query.common.Project;
 import taiga.model.query.sprint.Sprint;
 import taiga.model.query.sprint.UserStory;
 
@@ -19,6 +18,13 @@ public class ScopeChangeCalculator {
         this.historyAPI = new HistoryAPI();
     }
 
+    /**
+     * Makes api calls to the HistoryAPI to find out if a sprint's user story was added before or after the start date
+     * 
+     * @param userStory The userstory being analyzed
+     * @param sprint The sprint the userstory belongs too
+     * @return The date the userstory was added if added after the sprint start, else returns null.
+     */
     protected Date getAddedAfterStartDate(UserStory userStory, Sprint sprint) {
         AtomicReference<Date> addedDate = new AtomicReference<>();
         historyAPI.getUserStoryHistory(userStory.getId(), result -> {
@@ -41,11 +47,13 @@ public class ScopeChangeCalculator {
         return addedDate.get();
     }
 
-    public List<ScopeChangeItem> calculate(Project project, Sprint sprint) {
-        return calculate(project.getId(), sprint);
-    }
-
-    public List<ScopeChangeItem> calculate(int projectId, Sprint sprint) {
+    /**
+     * Analyzes the sprints User Stories and returns a list of those that were added after the start of the sprint
+     * 
+     * @param sprint The Sprint to analyze for scope change
+     * @return A list of ScopeChangeItems which contain the added UserStories
+     */
+    public List<ScopeChangeItem> calculate(Sprint sprint) {
         List<UserStory> stories = sprint.getUserStories();
 
         List<ScopeChangeItem> addedAfterStart = stories
