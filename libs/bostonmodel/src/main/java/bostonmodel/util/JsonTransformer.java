9c16582ae2fdf56fd90b7630a96a63f5350551fc
mjkangas1 <mjkangas@asu.edu>
2024-04-08 20:17:30 -0700

    T-380 Added JsonTransformer for service messaging

---
+++ libs/bostonmodel/src/main/java/bostonmodel/util/JsonTransformer.java
@@ -0,0 +1,12 @@
+import com.google.gson.Gson;
+
+public class JsonTransformer implements ResponseTransformer {
+
+    private Gson gson = new Gson();
+
+    @Override
+    public String render(Object model) {
+        return gson.toJson(model);
+    }
+
+}
