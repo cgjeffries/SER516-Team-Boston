76cf10d923bd4575718d0e0267d5c467a888525d
mayowaaaaa <oesan@asu.edu>
2024-04-13 22:34:49 -0700

    Task 427: add api endpoints

--- services/taskinertia/src/main/java/taskinertia/Main.java
+++ services/taskinertia/src/main/java/taskinertia/Main.java
@@ -1,7 +1,53 @@
 package taskinertia;
 
-public class Main {
-    public static void main(String[] args) {
+import bostonmodel.util.JsonTransformer;
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import serviceutil.Env;
 
+import java.time.LocalDate;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        port(Env.getPort());
+        get("/taskinertia", (request, response) -> {
+            response.type("application/json");
+
+            int projectId;
+            LocalDate start;
+            LocalDate end;
+
+            try {
+                projectId = Integer.parseInt(request.queryParams("project_id"));
+            } catch (NumberFormatException ex) {
+                response.status(HttpStatus.SC_BAD_REQUEST);
+                logger.error("project_id must be an integer");
+                return "";
+            }
+
+            try {
+                start = LocalDate.parse(request.queryParams("start_date"));
+            } catch (NumberFormatException ex) {
+                response.status(HttpStatus.SC_BAD_REQUEST);
+                logger.error("start_date must be a date in the format 'YYYY-MM-DD'");
+                return "";
+            }
+
+            try {
+                end = LocalDate.parse(request.queryParams("end_date"));
+            } catch (NumberFormatException ex) {
+                response.status(HttpStatus.SC_BAD_REQUEST);
+                logger.error("end_date must be a date in the format 'YYYY-MM-DD'");
+                return "";
+            }
+
+            return TaskInertiaCalculator.calculate(response, projectId, start, end);
+        }, new JsonTransformer());
     }
 }
