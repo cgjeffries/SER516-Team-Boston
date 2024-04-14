b5787946c5a832afec5f85f133b112fe14d2d6eb
mayowaaaaa <oesan@asu.edu>
2024-04-14 15:46:18 -0700

    Task 435: wip router (needs dockerfile setup)

--- services/router/src/main/java/router/routes/Routes.java
+++ services/router/src/main/java/router/routes/Routes.java
@@ -3,11 +3,13 @@ package router.routes;
 import java.util.List;
 
 import router.routes.pbhealth.PBHealthRoute;
+import router.routes.taskexcess.TaskExcessRoute;
 
 public class Routes {
     public static List<Route> getAll() {
         return List.of(
-                new PBHealthRoute()
+                new PBHealthRoute(),
+                new TaskExcessRoute()
         );
     }
 }
