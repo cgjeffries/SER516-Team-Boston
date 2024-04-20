0e6c2667a331b03698640a2267c41b313f0b0819
mayowaaaaa <oesan@asu.edu>
2024-04-20 11:20:46 -0700

    Task 481: add api to bostonclient

--- libs/bostonclient/src/main/java/bostonclient/BostonClient.java
+++ libs/bostonclient/src/main/java/bostonclient/BostonClient.java
@@ -1,15 +1,22 @@
 package bostonclient;
 
 import bostonclient.apis.PBHealthAPI;
+import bostonclient.apis.TaskInertiaAPI;
 
 public class BostonClient {
     private static PBHealthAPI pbHealthAPI;
+    private static TaskInertiaAPI taskInertiaAPI;
 
     public static void buildClient(BostonClientOptions options) {
         pbHealthAPI = new PBHealthAPI(options.getRouterUrl());
+        taskInertiaAPI = new TaskInertiaAPI(options.getRouterUrl());
     }
 
     public static PBHealthAPI getPBChangeAPI() {
         return pbHealthAPI;
+    }
+
+    public static TaskInertiaAPI getTaskInertiaAPI() {
+        return taskInertiaAPI;
     }
 }
