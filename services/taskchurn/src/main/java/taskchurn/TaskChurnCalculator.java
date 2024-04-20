5ab8e5b3c304e1b7bbf06eccaf25c5aaac59f006
cgjeffri <cgjeffri@asu.edu>
2024-04-20 01:50:36 -0700

    Task-421: add logic for task churn (still needs to be changed to switch to absolute counts instead of percent) (WIP)

--- services/taskchurn/src/main/java/taskchurn/TaskChurnCalculator.java
+++ services/taskchurn/src/main/java/taskchurn/TaskChurnCalculator.java
@@ -1,91 +1,127 @@
 package taskchurn;
 
+import bostonmodel.scopechange.ScopeChangeItem;
+import bostonmodel.taskchurn.TaskChurnItem;
+import java.lang.reflect.Array;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import java.util.concurrent.atomic.AtomicReference;
 import org.apache.http.HttpStatus;
 
 import serviceutil.DateUtil;
 import spark.Response;
 import taiga.TaigaClient;
+import taiga.models.sprint.Sprint;
 import taiga.models.taskhistory.ItemHistory;
 import taiga.models.taskhistory.ItemHistoryValuesDiff;
 import taiga.models.tasks.Task;
 
 public class TaskChurnCalculator {
-//    public static TaskChurnMetrics calculate(Response response, int projectId, LocalDate startDate, LocalDate endDate) {
-//
-//        List<Task> tasks = new ArrayList<>();
-//        TaigaClient.getTasksAPI().listTasksByProject(projectId, result -> {
-//            if (result.getStatus() == HttpStatus.SC_OK) {
-//                tasks.addAll(List.of(result.getContent()));
-//            }
-//        }).join();
-//
-//        if (tasks.isEmpty()) {
-//            response.status(HttpStatus.SC_BAD_REQUEST);
-//            return null;
-//        }
-//
-//        TreeMap<LocalDate, Integer> taskCounts = new TreeMap<>();
-//        TreeMap<LocalDate, Double> inertia = new TreeMap<>();
-//
-//        tasks
-//                .parallelStream()
-//                .forEach(t -> {
-//                    List<ItemHistory> taskHistory = new ArrayList<>();
-//                    TaigaClient.getTaskHistoryAPI().getTaskHistory(t.getId(), result -> {
-//                        if (result.getStatus() == HttpStatus.SC_OK) {
-//                            taskHistory.addAll(List.of(result.getContent()));
-//                        }
-//                    }).join();
-//                    AtomicBoolean moved = new AtomicBoolean(false);
-//                    taskHistory.forEach(entry -> {
-//                        ItemHistoryValuesDiff valuesDiff = entry.getValuesDiff();
-//                        if (valuesDiff.getStatus() == null) {
-//                            return;
-//                        }
-//                        if (!moved.get()) {
-//                            Integer currentDateCount = taskCounts.get(DateUtil.toLocal(entry.getCreatedAt()));
-//                            if (currentDateCount == null) {
-//                                currentDateCount = 0;
-//                            }
-//                            taskCounts.put(DateUtil.toLocal(entry.getCreatedAt()), currentDateCount + 1);
-//                            moved.set(true);
-//                        }
-//                    });
-//                });
-//
-//        for (LocalDate date = startDate; !date.isAfter(endDate); date = date.plusDays(1)) {
-//            int taskCount = totalTasksAtGivenDate(tasks, date);
-//            if (taskCounts.containsKey(date)) {
-//                double percent = ((double) (taskCount - taskCounts.get(date)) / taskCount);
-//                inertia.put(date, percent);
-//            } else {
-//                inertia.put(date, 1.0d);
-//            }
-//        }
-//
-//        response.status(HttpStatus.SC_OK);
-//        return new TaskInertiaMetrics(inertia);
-//    }
-//
-//    private static int totalTasksAtGivenDate(List<Task> tasks, LocalDate date) {
-//        return tasks
-//                .stream()
-//                .filter(t -> {
-//                    LocalDate createdDate = DateUtil.toLocal(t.getCreatedDate());
-//                    boolean createdBeforeDate = createdDate.isBefore(date) || createdDate.isEqual(date);
-//                    if (t.getFinishedDate() == null) {
-//                        return createdBeforeDate;
-//                    }
-//                    LocalDate finishedDate = DateUtil.toLocal(t.getCreatedDate());
-//                    return createdBeforeDate && finishedDate.isAfter(date) || finishedDate.isEqual(date);
-//                })
-//                .toList()
-//                .size();
-//    }
+    public static List<TaskChurnItem> calculate(Response response, int sprintId) {
+        TreeMap<LocalDate, Integer> taskCount = new TreeMap<>();
+        TreeMap<LocalDate, Integer> taskModifiedCount = new TreeMap<>();
+        TreeMap<LocalDate, Integer> taskChurn = new TreeMap<>();
+
+        AtomicReference<Sprint> sprintReference = new AtomicReference<>();
+        TaigaClient.getSprintAPI().getSprint(sprintId, result -> {
+            if (result.getStatus() != 200) {
+                return;
+            }
+            sprintReference.set(result.getContent());
+        }).join(); //TODO: parallelize this and below
+
+
+        AtomicReference<List<Task>> allTasksReference = new AtomicReference<>();
+        TaigaClient.getTasksAPI().listTasksByMilestone(sprintId,
+            result -> {
+                if (result.getStatus() != 200) {
+                    return;
+                }
+                allTasksReference.set(List.of(result.getContent()));
+            }).join();
+
+
+        List<Task> allTasks = allTasksReference.get();
+
+        List<ItemHistory> taskHistoryList = new ArrayList<>();
+        //find number of tasks per day in sprint
+        allTasks.parallelStream()
+            .forEach(task ->{
+                    AtomicReference<List<ItemHistory>> itemHistoryReference = new AtomicReference<>();
+                    TaigaClient.getTaskHistoryAPI().getTaskHistory(task.getId(), result -> {
+                        if (result.getStatus() != 200) {
+                            return;
+                        }
+                        itemHistoryReference.set(List.of(result.getContent()));
+                    }).join();
+                    taskHistoryList.addAll(itemHistoryReference.get());
+                });
+
+        for (Task task : allTasks) {
+            //populate task count
+            LocalDate createdDate = DateUtil.toLocal(task.getCreatedDate());
+            if (taskCount.containsKey(createdDate)) {
+                Integer temp = taskCount.get(createdDate);
+                taskCount.put(createdDate, ++temp);
+            } else {
+                taskCount.put(createdDate, 1);
+            }
+        }
+        Integer baseline = taskCount.get(taskCount.firstKey());
+        Integer dayOne = baseline;
+
+        for (ItemHistory taskHistory : taskHistoryList) {
+            if (taskHistory.getValuesDiff() != null) { //null check
+                ItemHistoryValuesDiff valuesDiff = taskHistory.getValuesDiff();
+                LocalDate createdAt = DateUtil.toLocal(taskHistory.getCreatedAt());
+                if (valuesDiff.getStatus() == null) {
+                    if (taskModifiedCount.containsKey(createdAt)) {
+                        Integer temp = taskModifiedCount.get(createdAt);
+                        taskModifiedCount.put(createdAt, ++temp);
+                    } else {
+                        taskModifiedCount.put(createdAt, 1);
+                    }
+                }
+            }
+        }
+
+        taskModifiedCount.put(taskCount.firstKey(), 0);
+        TreeSet<LocalDate> allKeys = new TreeSet<>();
+        allKeys.addAll(taskCount.keySet());
+        allKeys.addAll(taskModifiedCount.keySet());
+        //TODO: Fix this to be a count of changes rather than a percentage
+        for (LocalDate date : allKeys) {
+
+            Integer churn = (int) (((float) (Math.abs(taskCount.getOrDefault(date, 0) - baseline) + taskModifiedCount.getOrDefault(date, 0)) / (float) dayOne) * 100);
+            if (taskCount.get(date) != null) {
+                baseline = taskCount.get(date);
+            }
+            taskChurn.put(date, Math.abs(churn));
+        }
+
+
+        //put days with no churn to 0
+        LocalDate start = DateUtil.toLocal(sprintReference.get().getEstimatedStart());
+        LocalDate end = DateUtil.toLocal(sprintReference.get().getEstimatedFinish());
+        List<LocalDate> dates = start.datesUntil(end.plusDays(1)).toList();
+
+        for(LocalDate date : dates){
+            if(!taskChurn.containsKey(date)){
+                taskChurn.put(date, 0);
+            }
+        }
+
+        List<TaskChurnItem> taskChurnItemList = new ArrayList<>();
+        for(LocalDate date : taskChurn.keySet()){
+            taskChurnItemList.add(new TaskChurnItem(date, taskChurn.get(date)));
+        }
+        return taskChurnItemList;
+    }
 }
