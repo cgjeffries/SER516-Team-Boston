eb33b6ea5dc6a1b0ae0fc700e6fc3b6c7fa00e2e
cgjeffri <cgjeffri@asu.edu>
2024-04-21 00:32:21 -0700

    Task-488: connected task churn service to task churn api

--- client/src/main/java/ui/services/TaskChurnService.java
+++ client/src/main/java/ui/services/TaskChurnService.java
@@ -1,12 +1,30 @@
 package ui.services;
 
+import bostonclient.BostonClient;
+import bostonmodel.taskchurn.TaskChurnItem;
+import bostonmodel.taskchurn.TaskChurnMetrics;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import taiga.models.sprint.Sprint;
 
 public class TaskChurnService extends Service<Object> {
     private Sprint sprint;
+
+    private final ObservableList<TaskChurnItem> taskChurnItems;
+
+    public TaskChurnService() {
+        this.taskChurnItems = FXCollections.observableArrayList();
+    }
+
+    public ObservableList<TaskChurnItem> getTaskChurnItems(){
+        return taskChurnItems;
+    }
+
     public void recalculate(Sprint sprint) {
         if (sprint != null && sprint.getId() > 0) {
             this.sprint = sprint;
@@ -15,6 +33,30 @@ public class TaskChurnService extends Service<Object> {
     }
     @Override
     protected Task<Object> createTask() {
-        return null;
+        return new Task<>() {
+            @Override
+            protected Object call() {
+                if (sprint == null) {
+                    return null;
+                }
+                AtomicReference<TaskChurnMetrics> metricsReference = new AtomicReference<>();
+                BostonClient.getTaskChurnAPI().getTaskChurn(sprint.getId(), result -> {
+                    if (result.getStatus() != 200) {
+                        System.out.println("Error: TaskChurn service returned bad response code: " +
+                            result.getStatus());
+                        metricsReference.set(new TaskChurnMetrics(
+                            new ArrayList<>())); //set to empty (instead of null) so things don't explode
+                    }
+                });
+
+                TaskChurnMetrics taskChurnMetrics = metricsReference.get();
+
+                Platform.runLater(() -> {
+                    taskChurnItems.setAll(taskChurnMetrics.getTaskChurnItems());
+                });
+
+                return null;
+            }
+        };
     }
 }
