516ce62ca8d0ebe14cc4b46353254e1871fc356b
pranawwwww <tknolast@asu.edu>
2024-04-21 02:10:20 -0700

    T:485 adding UI elements

--- client/src/main/java/ui/metrics/taskchurn/TaskChurn.java
+++ client/src/main/java/ui/metrics/taskchurn/TaskChurn.java
@@ -1,5 +1,13 @@
 package ui.metrics.taskchurn;
 
+import javafx.beans.binding.Bindings;
+import javafx.collections.ObservableList;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.StackPane;
 import taiga.models.sprint.Sprint;
 import ui.services.TaskChurnService;
@@ -14,7 +22,33 @@ public class TaskChurn extends StackPane {
     }
 
     private void init() {
-        //TODO
+        
+        CategoryAxis dateAxis = new CategoryAxis();
+        dateAxis.setLabel("Date");
+        NumberAxis percentAxis = new NumberAxis();
+        percentAxis.setLabel("Task Churn (%)");
+
+        LineChart<String, Number> chart = new LineChart<>(dateAxis, percentAxis);
+        chart.visibleProperty().bind(this.service.runningProperty().not());
+        chart.setAnimated(false);
+
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Task Churn (%)");
+        // series.dataProperty().bind(Bindings.createObjectBinding(() ->
+        //     this.service.getTaskChurnItems()
+        series.dataProperty().addListener(observable -> {
+            for (final XYChart.Data<String, Number> data : series.getData()) {
+                Tooltip.install(data.getNode(), new Tooltip(String.format("Churn: %.2f%%", (Double) data.getYValue())));
+            }
+        });
+        chart.getData().addAll(series);
+
+        ProgressIndicator progress = new ProgressIndicator(-1d);
+        progress.visibleProperty().bind(this.service.runningProperty());
+
+        StackPane root = new StackPane(chart, progress);
+
+        getChildren().add(root);
     }
 
     public TaskChurnService getService() {
