40e1ee5e1e35d61f54d89878991e6ae60c871cd0
jskim26 <jskim26@asu.edu>
2024-04-20 18:25:42 -0700

    proper ui flow task484

--- client/src/main/java/ui/metrics/taskDefectDensity/TaskDefectDensity.java
+++ client/src/main/java/ui/metrics/taskDefectDensity/TaskDefectDensity.java
@@ -1,5 +1,58 @@
 package ui.metrics.taskDefectDensity;
 
-public class TaskDefectDensity {
-    
+import javafx.geometry.Pos;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+import ui.services.TaskDefectDensityService;
+import javafx.scene.chart.PieChart;
+
+
+public class TaskDefectDensity extends StackPane {
+    private final TaskDefectDensityService service;
+    private PieChart pieChart; // moving hard-coded pie over
+
+    public TaskDefectDensity() {
+        this.service = new TaskDefectDensityService();
+        this.init();
+    }
+
+    private void init() {
+        VBox.setVgrow(this, Priority.ALWAYS);    
+        StackPane root = new StackPane();
+        VBox.setVgrow(root, Priority.ALWAYS);
+
+        pieChart = new PieChart();
+
+        pieChart.setTitle("Task Defect Density");
+        pieChart.getData().addAll(
+            new PieChart.Data("Finished", 30),
+            new PieChart.Data("Unfinished", 20),
+            new PieChart.Data("Deleted", 50)
+        );
+
+        VBox container = new VBox(
+            pieChart
+        );
+        container.setPrefHeight(VBox.USE_PREF_SIZE);
+        container.setAlignment(Pos.CENTER);
+        container.visibleProperty().bind(this.service.runningProperty().not());
+        VBox.setVgrow(container, Priority.ALWAYS);
+
+        ProgressIndicator progress = new ProgressIndicator(-1d);
+        progress.visibleProperty().bind(this.service.runningProperty());
+
+        root.getChildren().add(progress);
+        root.getChildren().add(container);
+
+        getChildren().add(root);
+
+        this.service.start();
+    }
+
+    public void calculate(int projectId) {
+        this.service.recalculate(projectId);
+    }
 }
--- client/src/main/java/ui/screens/TaskDefectDensityScreen.java
+++ client/src/main/java/ui/screens/TaskDefectDensityScreen.java
@@ -1,57 +1,42 @@
 package ui.screens;
 
-import javafx.scene.chart.PieChart;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import settings.Settings;
 import ui.components.screens.ScreenManager;
-import ui.services.TaskDefectDensityService;
+import ui.metrics.taskDefectDensity.TaskDefectDensity;
 
 public class TaskDefectDensityScreen extends BaseMetricConfiguration {
-    private PieChart pieChart;
-    //Needs to be used and potentiall replace piechart as main element
-    private TaskDefectDensityService taskDefectDensity;
+    private TaskDefectDensity taskDD;
 
     public TaskDefectDensityScreen(ScreenManager screenManager, String id, String fxmlFilename) {
         super(screenManager, id, fxmlFilename);
-        initializeUI();
-    }
-
-    private void initializeUI() {
-        //TODO PieChart still hardcoded, need service/class to call update? Weird as service has no params
-        pieChart = new PieChart();
-        pieChart.setTitle("Task Defect Density");
-        pieChart.getData().addAll(
-            new PieChart.Data("Finished", 30),
-            new PieChart.Data("Unfinished", 20),
-            new PieChart.Data("Deleted", 50)
-        );
-        if (taskDefectDensity != null) {
-            taskDefectDensity.recalculate(Settings.get().getAppModel().getCurrentProject().get().getId());
-        }
-        // hideSprintParameter(); Keeps breaking app, need a second set of eyes.
-        this.taskDefectDensity = new TaskDefectDensityService();
     }
 
     @Override
+    protected void beforeVisualizationMount() {
+    }
+
+
+    @Override
     protected Pane visualization() {
-        StackPane pane = new StackPane(pieChart);
-        return pane;
-        //TODO Needs to return the churn class when connected, but might only need service
+        return this.taskDD;
+    }
+
+    @Override
+    protected void beforeParameterMount() {
+        this.taskDD = new TaskDefectDensity();
     }
 
     @Override
     protected Pane parameters() {
-        // Currently defect density does task info all at once? need a second set of eyes. Might have no parameters in current state
         return null;
-
     }
 
     @Override
     protected void onFocused() {
-        if (this.taskDefectDensity == null) {
+        if (this.taskDD == null) {
             return;
         }
-        taskDefectDensity.recalculate(Settings.get().getAppModel().getCurrentProject().get().getId());
+        this.taskDD.calculate(Settings.get().getAppModel().getCurrentProject().get().getId());
     }
 }
