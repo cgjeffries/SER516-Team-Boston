2d05598fc4695bcff218f39e02646f85c9fd840e
cgjeffri <cgjeffri@asu.edu>
2024-04-21 18:56:44 -0700

    Task-504: added PBChange to bostonAPI

--- libs/bostonclient/src/main/java/bostonclient/BostonClient.java
+++ libs/bostonclient/src/main/java/bostonclient/BostonClient.java
@@ -1,24 +1,30 @@
 package bostonclient;
 
+import bostonclient.apis.PBChangeAPI;
 import bostonclient.apis.PBHealthAPI;
 import bostonclient.apis.TaskChurnAPI;
 import bostonclient.apis.TaskExcessAPI;
 import bostonclient.apis.TaskInertiaAPI;
 
 public class BostonClient {
+    private static PBChangeAPI pbChangeAPI;
     private static PBHealthAPI pbHealthAPI;
     private static TaskChurnAPI taskChurnAPI;
     private static TaskExcessAPI taskExcessAPI;
     private static TaskInertiaAPI taskInertiaAPI;
 
     public static void buildClient(BostonClientOptions options) {
+        pbChangeAPI = new PBChangeAPI(options.getRouterUrl());
         pbHealthAPI = new PBHealthAPI(options.getRouterUrl());
         taskChurnAPI = new TaskChurnAPI(options.getRouterUrl());
         taskExcessAPI = new TaskExcessAPI(options.getRouterUrl());
         taskInertiaAPI = new TaskInertiaAPI(options.getRouterUrl());
     }
 
-    public static PBHealthAPI getPBChangeAPI() {
+    public static PBChangeAPI getPbChangeAPI() {
+        return pbChangeAPI;
+    }
+    public static PBHealthAPI getPBHealthAPI() {
         return pbHealthAPI;
     }
     public static TaskChurnAPI getTaskChurnAPI(){
