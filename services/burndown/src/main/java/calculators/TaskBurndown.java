17b880a8a95242d8f3caf8d283dd86b047b7156b
mayowaaaaa <oesan@asu.edu>
2024-04-21 18:17:13 -0700

    Task 353: move logic to service

---
+++ services/burndown/src/main/java/calculators/TaskBurndown.java
@@ -0,0 +1,32 @@
+package calculators;
+
+import bostonmodel.burndown.BurnDownEntry;
+import taiga.TaigaClient;
+import taiga.models.sprint.Days;
+import taiga.models.sprint.Sprint;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class TaskBurndown implements BurndownCalculator {
+
+    @Override
+    public List<BurnDownEntry> calculate(Sprint sprint) {
+        AtomicReference<List<Days>> days = new AtomicReference<>();
+
+        CompletableFuture<Void> future = TaigaClient.getSprintStatsAPI().getSprintStats(sprint.getId(), result -> {
+            days.set(List.of(result.getContent().getDays()));
+        });
+
+        future.join(); // wait for the request to complete
+
+        List<BurnDownEntry> burndown = new ArrayList<>();
+
+        days.get().forEach(
+                d -> burndown.add(new BurnDownEntry(Math.max(0, d.getOptimalPoints()), d.getOpenPoints(), d.getDay())));
+
+        return burndown;
+    }
+}
